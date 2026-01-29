f1a464b9d1609d90b6ce31432e369effb5145e03
Constantine Strokin <strokinconstantine@gmail.com>
2026-01-29 13:29:02 +0000

    feature

--- cmake/generate_cpp.cmake
+++ cmake/generate_cpp.cmake
@@ -1,13 +1,13 @@
-set(zserio_types_cpp_genpath ${CMAKE_CURRENT_BINARY_DIR}/include)
-
 include("$ENV{ZSERIO_CPP_RUNTIME}/../../cmake/zserio_compiler.cmake")
 
 set(ZSERIO_JAR_FILE $ENV{ZSERIO_JAR})
 
+set(ZSERIO_GENERATED_CPP_FILES_PATH ${CMAKE_CURRENT_BINARY_DIR}/include)
+
 zserio_generate_cpp(
     TARGET ${PROJECT_NAME}
     MAIN_ZS "euroradio_asc/subset_026/messages.zs"
-    GEN_DIR ${zserio_types_cpp_genpath}
+    GEN_DIR ${ZSERIO_GENERATED_CPP_FILES_PATH}
     GENERATED_SOURCES_VAR ZSERIO_GENERATED_SOURCES
 )
 
